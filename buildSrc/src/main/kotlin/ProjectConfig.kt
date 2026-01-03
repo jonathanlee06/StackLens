0a48c47df394caf470b2a806acfaa5951e9d3d52
Jonathan <devbyjonathan@gmail.com>
2026-01-03 20:09:33 +0800

    chore: bump app version (v0.0.4)

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -3,6 +3,6 @@ object ProjectConfig {
     const val minSdk = 21
     const val targetSdk = 36
     const val namespace = "com.devbyjonathan.stacklens"
-    const val versionCode = 2
-    const val versionName = "0.0.3-alpha"
+    const val versionCode = 4
+    const val versionName = "0.0.4-alpha"
 }
